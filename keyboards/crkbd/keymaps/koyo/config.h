f571312f77a0e6e3410e0ef0e6c4b9cebf8d25d7
nbr <nikolaus.brunner@protonmail.ch>
2024-08-15 14:17:03 +0200

    [crkbd.koyo]: try out PERMISSIVE_HOLD

--- keyboards/crkbd/keymaps/koyo/config.h
+++ keyboards/crkbd/keymaps/koyo/config.h
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 #define TAPPING_TERM 150
 #define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLD
 #define COMBO_TERM 35
 
 #ifdef RGBLIGHT_ENABLE
